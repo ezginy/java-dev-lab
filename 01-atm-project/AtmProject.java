0f31fa3186538b2d842f41bfdb78dcdd4336f1ec
Ezgi <ezginurryigit@gmail.com>
2026-06-26 05:35:31 +0300

    chore: rename Main to AtmProject for better naming convention

--- 01-atm-project/AtmProject.java
+++ 01-atm-project/AtmProject.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class AtmProject {
 
     /**
      * Prompts the user to continue or exit the application.
---
+++ 03-crud-manager/CrudManager.java
@@ -0,0 +1,5 @@
+public class CrudManager {
+    public static void main(String[] args) {
+        System.out.print("Welcome to the CRUD Manager!");
+    }
+}
