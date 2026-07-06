86646165cfe3c772677a1b17d1fb6a362258b0b9
Ezgi Nur Yigit <ezgi@E-yLAPTOP.localdomain>
2026-07-06 22:19:43 +0300

    feat: remove crud-manager draft and start rpg-battle-simulator project

--- 03-crud-manager/CrudManager.java
+++
@@ -1,5 +0,0 @@
-public class CrudManager {
-    public static void main(String[] args) {
-        System.out.print("Welcome to the CRUD Manager!");
-    }
-}
---
+++ 03-rpg-battle-simulator/Character.java
@@ -0,0 +1,5 @@
+public class Character {
+    public static void c(){
+        System.out.println("Hello");
+    }
+}
